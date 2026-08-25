729279eca4f9e7534da20c41b9f9789c9013aad1
Bismillah Bazaar Admin <admin@bismillahbazaar.com>
2026-08-25 19:03:46 -0400

    fix: add CartProvider to app layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,18 +3,21 @@ import { BottomTabBar } from "@/components/bottom-tab-bar";
 import { CartButton } from "@/components/cart-button";
 import { ToastContainer } from "@/components/toast-container";
 import { NotificationPoller } from "@/components/notification-poller";
+import { CartProvider } from "@/components/cart-provider";
 
 export default function AppLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex min-h-dvh w-full flex-col bg-zinc-50">
-      <AppHeader />
-      <main className="flex-1 px-4 pb-32 pt-4 sm:px-6 lg:px-8">{children}</main>
-      <BottomTabBar />
-      <CartButton />
-      <ToastContainer />
-      <NotificationPoller />
-    </div>
+    <CartProvider>
+      <div className="flex min-h-dvh w-full flex-col bg-zinc-50">
+        <AppHeader />
+        <main className="flex-1 px-4 pb-32 pt-4 sm:px-6 lg:px-8">{children}</main>
+        <BottomTabBar />
+        <CartButton />
+        <ToastContainer />
+        <NotificationPoller />
+      </div>
+    </CartProvider>
   );
 }
